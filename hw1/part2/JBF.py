8020f014b725ec29ea12ed8d81dda8cd6ff7c5fa
Codeamon <CodeamonCat@gmail.com>
2024-04-12 15:50:38 +0800

    finished hw1 part 2

--- hw1/part2/JBF.py
+++ hw1/part2/JBF.py
@@ -1,20 +1,53 @@
-
 import numpy as np
 import cv2
+from tqdm import trange
 
 
 class Joint_bilateral_filter(object):
+
     def __init__(self, sigma_s, sigma_r):
         self.sigma_r = sigma_r
         self.sigma_s = sigma_s
-        self.wndw_size = 6*sigma_s+1
-        self.pad_w = 3*sigma_s
-    
+        self.wndw_size = 6 * sigma_s + 1
+        self.pad_w = 3 * sigma_s
+
     def joint_bilateral_filter(self, img, guidance):
         BORDER_TYPE = cv2.BORDER_REFLECT
-        padded_img = cv2.copyMakeBorder(img, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE).astype(np.int32)
-        padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE).astype(np.int32)
-
+        padded_img = cv2.copyMakeBorder(img, self.pad_w, self.pad_w,
+                                        self.pad_w, self.pad_w,
+                                        BORDER_TYPE).astype(np.int32)
+        padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w,
+                                             self.pad_w, self.pad_w,
+                                             BORDER_TYPE).astype(np.int32)
         ### TODO ###
-        
+        # G_s(p, q) = e ^ (-(((x_p - x_q)^2 + (y_p - y_q)^2) / (2 * sigma_s ^ 2)))
+        table_G_s = np.exp(-(np.arange(self.pad_w + 1)**2) /
+                           (2 * self.sigma_s**2))
+
+        # T is single-channel:  G_r(T_p, T_q) = e ^ (-((T_p-T_q)^2 / (2 * sigma_s ^ 2)))
+        # T is color image:     G_r(T_p, T_q) = e ^ (-(((T_p^r-T_q^r)^2+(T_p^q-T_q^q)^2+(T_p^b-T_q^b)^2) / (2 * sigma_s ^ 2)))
+        table_G_r = np.exp(-((np.arange(256) / 255)**2 /
+                             (2 * self.sigma_r**2)))
+
+        hwc = padded_img.shape
+        weight = np.zeros(hwc)
+        result = np.zeros(hwc)
+
+        for x in trange(-self.pad_w, self.pad_w + 1):
+            for y in range(-self.pad_w, self.pad_w + 1):
+                dT = table_G_r[np.abs(
+                    np.roll(padded_guidance, [y, x], axis=[0, 1]) -
+                    padded_guidance)]
+                r_weight = dT if dT.ndim == 2 else np.prod(dT, axis=2)
+                s_weight = table_G_s[np.abs(x)] * table_G_s[np.abs(y)]
+                t_weight = s_weight * r_weight
+                padded_img_roll = np.roll(padded_img, [y, x], axis=[0, 1])
+                for channel in range(padded_img.ndim):
+                    result[:, :,
+                           channel] += padded_img_roll[:, :,
+                                                       channel] * t_weight
+                    weight[:, :, channel] += t_weight
+        output = (result / weight)[self.pad_w:-self.pad_w,
+                                   self.pad_w:-self.pad_w, :]
+
         return np.clip(output, 0, 255).astype(np.uint8)
--- hw1/part2/main.py
+++ hw1/part2/main.py
@@ -1,21 +1,66 @@
 import numpy as np
+import pandas as pd
 import cv2
 import argparse
 import os
 from JBF import Joint_bilateral_filter
 
 
+def read_settings(setting_path: str) -> tuple[list, int, float]:
+    with open(setting_path, "r", encoding="UTF-8") as f:
+        setting = [line.rstrip('\n').split(',') for line in f.readlines()]
+    f.close()
+    return setting[1:6], int(setting[6][1]), float(setting[6][3])
+
+
 def main():
-    parser = argparse.ArgumentParser(description='main function of joint bilateral filter')
-    parser.add_argument('--image_path', default='./testdata/1.png', help='path to input image')
-    parser.add_argument('--setting_path', default='./testdata/1_setting.txt', help='path to setting file')
+    parser = argparse.ArgumentParser(
+        description='main function of joint bilateral filter')
+    parser.add_argument('--image_path',
+                        default='./testdata/2.png',
+                        help='path to input image')
+    parser.add_argument('--setting_path',
+                        default='./testdata/2_setting.txt',
+                        help='path to setting file')
     args = parser.parse_args()
 
     img = cv2.imread(args.image_path)
-    img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     ### TODO ###
+    RGB_params, sigma_s, sigma_r = read_settings(args.setting_path)
+    JBF = Joint_bilateral_filter(sigma_s, sigma_r)
+    img_BF = JBF.joint_bilateral_filter(img_rgb, img_rgb)
+    img_JBF = JBF.joint_bilateral_filter(img_rgb, img_gray)
+
+    cost = dict()
+    cost['BGR2GRAY'] = np.sum(
+        np.abs(img_BF.astype('int32') - img_JBF.astype('int32')))
+    # save image
+    cv2.imwrite(
+        f'./result/{args.image_path[-5:-4]}_image_cv2_COLOR_BGR2GRAY.png',
+        img_gray)
+    cv2.imwrite(f'./result/{args.image_path[-5:-4]}_BF.png', img_BF)
+    cv2.imwrite(f'./result/{args.image_path[-5:-4]}_JBF.png', img_JBF)
+
+    # 6 gray-scale images
+    for R, G, B in RGB_params:
+        # rgb to gray
+        img_param_gray = img_rgb[:, :, 0] * float(
+            R) + img_rgb[:, :, 1] * float(G) + img_rgb[:, :, 2] * float(B)
+        img_param_JBF = JBF.joint_bilateral_filter(img_rgb, img_param_gray)
+        cost[f'RGB_{R}_{G}_{B}'] = np.sum(
+            np.abs(img_BF.astype('int32') - img_param_JBF.astype('int32')))
+        img_param_JBF = cv2.cvtColor(img_param_JBF, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(
+            f'./result/{args.image_path[-5:-4]}_RGB_{R}_{G}_{B}_gray.png',
+            img_param_gray)
+        cv2.imwrite(
+            f'./result/{args.image_path[-5:-4]}_RGB_{R}_{G}_{B}_JBF.png',
+            img_param_JBF)
+    pd.Series(cost, name='Cost(1.pmg)').to_excel(
+        f'./result/{args.image_path[-5:-4]}_cost_table.xlsx')
 
 
 if __name__ == '__main__':
